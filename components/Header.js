48e231d48ef41dda16e58ad03b34eab637739f70
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-08-15 15:59:57 -0300

    add image e expo icons

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,47 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { Image } from 'expo-image';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function Header() {
     return (
         <View style={styles.header}>
-            <Text>Header</Text>
+            <View style={styles.user}>
+                <Image
+                    style={styles.avatar}
+                    source="https://cursinhoparamedicina.com.br/wp-content/uploads/2022/10/Paisagem-1.jpg"
+                />
+                <Text style={styles.name}>Alice Custódio</Text>
+            </View>
+            <Ionicons style={styles.menu} name="menu" size={24} color="black" />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header:{
-      flex: 1,
-      backgroundColor: 'brown',
-      justifyContent: 'center',
-      alignItems: 'center'
+    header: {
+        flex: 1,
+        // backgroundColor: 'brown',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
+    },
+    user:{
+        flexDirection:'row',
+        alignItems: 'center',
+        gap: 10
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 25
+    },
+    name: {
+        fontWeight: '600',
+        fontSize: 18
+    },
+    menu: {
+        padding: 10
     }
 })
