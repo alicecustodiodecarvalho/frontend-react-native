f84ae6562b02e59b04b99401767c3a691e98ccc2
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-08-29 17:35:19 -0300

    get accounts from api

--- components/Content.js
+++ components/Content.js
@@ -1,42 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native'
-import CardAccount from './CardAccount.js'
+import { useEffect, useState } from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+import CardAccount from './CardAccount'
 
 export default function Content() {
 
-    const accounts = [
-        {
-            id:1,
-            service: "Gmail",
-            imgUrl: "https://static.vecteezy.com/system/resources/previews/022/484/516/original/google-mail-gmail-icon-logo-symbol-free-png.png",
-            userName: "alicecustodio1908@gmail.com"
-        },
-        {
-            id:2,
-            service: "Instagram",
-            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/640px-Instagram_logo_2022.svg.png",
-            userName: "alicee_custodio"
-        },
-        {
-            id:3,
-            service: "Github",
-            imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/640px-GitHub_Invertocat_Logo.svg.png",
-            userName: "alicecustodiodecarvalho"
+    const [accounts, setAccounts] = useState([])
+
+    useEffect(() => {
+        const getAccounts = async () => {
+            const response = await fetch('http://localhost:3000/account/list')
+            if (response.ok) {
+                const data = await response.json()
+                console.log(data)
+                setAccounts(data.accounts)
+                return
+            }
+            console.log('Erro ao carregar accounts')
+            return
         }
-    ]
+
+        getAccounts()
+    }, [])
+
 
     return (
         <View style={styles.content}>
+
+
+            {accounts.length === 0 && <Text>Loading...</Text>}
+
             {
                 accounts.map((account) =>
                     <CardAccount
                         key={account.id}
                         service={account.service}
-                        imgUrl={account.imgUrl}
-                        userName={account.userName}
+                        imgUrl={account.logo_image}
+                        userName={account.username}
                     />
                 )
             }
-
         </View>
     )
 }
