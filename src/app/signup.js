d7a1965f3f023f56ee86469ef7e47a99c4f81998
alicecustodiodecarvalho <alice.custodio@aluno.ifsp.edu.br>
2024-10-22 17:51:34 -0300

    add sign up screen

--- src/app/signup.js
+++ src/app/signup.js
@@ -1,93 +1,96 @@
-import {View, StyleSheet, Text, TextInput} from 'react-native'
+import { View, StyleSheet, Text, TextInput, Alert } from 'react-native'
 import { useState } from "react"
 import Button from '../components/Button'
 import { useRouter } from 'expo-router'
-import { useAccountStore } from '../stores/useAccountStore'
 
-export default function SignUp(){
+export default function SignUp() {
 
-    const { addAccount } = useAccountStore()
     const router = useRouter()
 
-    const [txtServico, setTxtServico] = useState('')
-    const [txtUsername, setTxtUsername] = useState('')
+    const [txtName, setTxtName] = useState('')
+    const [txtEmail, setTxtEmail] = useState('')
+    const [txtAvatar, setTxtAvatar] = useState('')
     const [txtPass, setTxtPass] = useState('')
-    const [txtImgUrl, setTxtImgUrl] = useState('')
 
     const handleCreateAccount = async () => {
-        const account = {
-            service: txtServico,
-            username: txtUsername,
-            logo_image: txtImgUrl,
-            pass: txtPass,
-            user_id: 1
+        const user = {
+            name: txtName,
+            email: txtEmail,
+            avatar: txtAvatar,
+            pass: txtPass
         }
-    
-        const response = await fetch('http://localhost:3000/account', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(account)
+
+        const response = await fetch('http://localhost:3000/auth/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
         })
 
-        if(response.ok){
+        if (response.ok) {
             const data = await response.json()
-            addAccount(data.account)
+            Alert.alert('Usuario Criado com Sucesso!')
+            setTxtName('')
+            setTxtEmail('')
+            setTxtAvatar('')
+            setTxtPass('')
             router.back()
             return
+        } else {
+            const data = await response.json()
+            Alert.alert('Erro ao Criar Usuário')
+            console.log(data?.error)
         }
-
-        console.log('Erro ao carregar accounts')
         return
     }
 
-    return( 
+    return (
         <View style={styles.container}>
-          <Text style={{ fontSize: 26, marginVertical: 10 }}>Cadastre-se</Text>
-          <Text>Serviço:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtServico}
-            value={txtServico}
-            placeholder='Digite o nome do serviço...'
-            placeholderTextColor='#DDDDDD'
-          />
-          <Text>Username:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtUsername}
-            value={txtUsername}
-          />
-          <Text>Password:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtPass}
-            value={txtPass}
-          />
-          <Text>Logo URL:</Text>
-          <TextInput 
-            style={styles.input}
-            onChangeText={setTxtImgUrl}
-            value={txtImgUrl}
-            keyboardType='url'
-          />
-        <Button onPress={handleCreateAccount}>Cadastrar</Button>
+            <Text style={{ fontSize: 26, marginVertical: 10 }}>Cadastre-se</Text>
+            <Text>Name:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTxtName}
+                value={txtName}
+                placeholder='Digite seu nome...'
+                placeholderTextColor='#DDDDDD'
+            />
+            <Text>Email:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTxtEmail}
+                value={txtEmail}
+            />
+            <Text>Avatar URL:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTxtAvatar}
+                value={txtAvatar}
+                keyboardType='url'
+            />
+            <Text>Senha:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setTxtPass}
+                value={txtPass}
+            />
+            <Button onPress={handleCreateAccount}>Cadastrar</Button>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#444444',
-    paddingHorizontal: 10,
-    paddingVertical: 6,
-    marginVertical: 5,
-    borderRadius: 5
-  }
+    container: {
+        padding: 20
+    },
+    input: {
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: '#444444',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        marginVertical: 5,
+        borderRadius: 5
+    }
 })
